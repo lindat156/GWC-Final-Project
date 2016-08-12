53d78fe7cec71bf2ecdf62304f46fabb4ab5e9b0
briennac4rter <briennac4rter@gmail.com>
2016-08-12 09:44:05 -0400

    server is workin

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('finalproject.hbs');
 });
 
 module.exports = router;
