f1a9ba245ccb3143ad91c8e13a9a37daed8b14a1
briennac4rter <briennac4rter@gmail.com>
2016-08-12 09:48:12 -0400

    server stuff

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('finalproject.hbs');
 });
+router.get('/resources', function(req, res, next) {
+  res.render('rpage.hbs');
+});
 
 module.exports = router;
