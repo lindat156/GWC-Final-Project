79956f251d249d6c0e4dc03f08e0b14af57bdc87
briennac4rter <briennac4rter@gmail.com>
2016-08-12 12:05:46 -0400

    css link

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+module.exports = router;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,5 +12,43 @@ router.get('/resources', function(req, res, next) {
 router.get('/ourbeautifulmap', function(req, res, next) {
   res.render('AHH.hbs');
 });
+router.get('/chat', function(req, res, next) {
+  res.render('chat.hbs');
+});
 
-module.exports = router;
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/chat', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/chat.hbs');
+});
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// http.listen(3000, function(){
+//   console.log('listening on *:3000');
+// });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
