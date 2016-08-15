735360d0ea50ef472689b0a01db2d32104d5c8f7
briennac4rter <briennac4rter@gmail.com>
2016-08-15 09:39:44 -0400

    linking

--- routes/index.js
+++ routes/index.js
@@ -16,39 +16,3 @@ router.get('/chat', function(req, res, next) {
   res.render('chat.hbs');
 });
 
-
-var app = require('express')();
-var http = require('http').Server(app);
-
-app.get('/chat', function(req, res){
-  res.send('<h1>Hello world</h1>');
-});
-
-app.get('/chat', function(req, res){
-  res.sendFile(__dirname + '/chat.hbs');
-});
-
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-// http.listen(3000, function(){
-//   console.log('listening on *:3000');
-// });
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
